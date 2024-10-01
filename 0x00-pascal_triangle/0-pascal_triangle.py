d30d5bf290ee88be0eaadeb6955ab3e648c20bff
ilyassebou <bouaarrouda.ilyasse@gmail.com>
2024-10-01 13:48:13 +0000

    finished

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,16 +1,27 @@
 #!/usr/bin/python3
-"""Pascal Triangle Interview Challenge"""
+"""Pascal's Triangle"""
+
+
 def pascal_triangle(n):
-    # Return an empty list if n is less than or equal to 0
+    """returns a list of lists of numbers
+    representing the pascal triangle"""
+    tri = []
+
     if n <= 0:
         return []
 
-    triangle = []  # Initialize an empty list to hold the rows of the triangle
-    for i in range(n):  # Loop through each level of the triangle
-        row = [1] * (i + 1)  # Start with a row of 1's (length i + 1)
-        for j in range(1, i):  # Loop through the indices of the current row
-            # Calculate the value based on the two values above it in the previous row
-            row[j] = triangle[i - 1][j - 1] + triangle[i - 1][j]
-        triangle.append(row)  # Add the constructed row to the triangle
+    for i in range(n):
+        if (i == 0):
+            tri.append([1])
+        else:
+            row = []
+            for j in range(i + 1):
+                if (j == 0 or j == i):
+                    row.append(1)
+                else:
+                    row.append(tri[i - 1][j - 1] +
+                                   tri[i - 1][j])
 
-    return triangle  # Return the completed triangle
+            tri.append(row)
+
+    return (tri)
