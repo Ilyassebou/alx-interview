f3dd55e05da017604ccdde0e289f9ef9284980b7
ilyassebou <bouaarrouda.ilyasse@gmail.com>
2024-10-15 16:18:28 +0000

    Minimum Operations

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -1,16 +1,12 @@
 #!/usr/bin/python3
 """Minimum Operations"""
+
 def minOperations(n):
-    if n <= 1:
-        return 0
-
-    operations = 0
-    divisor = 2
-
+    nOpe = 0
+    minOpe = 2
     while n > 1:
-        while n % divisor == 0:
-            operations += divisor
-            n //= divisor
-        divisor += 1
-
-    return operations
+        while n % minOpe == 0:
+            nOpe += minOpe
+            n /= minOpe
+        minOpe += 1
+    return nOpe
